141b0415284f8a8d35b6563a1c67d58280a7c1d1
ethanzs <ethanzseligman@gmail.com>
2020-12-12 14:45:05 -0500

    fixing comments

--- client.py
+++ client.py
@@ -27,25 +27,29 @@ def send_data(s, msg):
     msg = "{:10}".format(len(msg)) + msg
     s.send(bytes(msg, "utf-8"))
 
+
 # get header size which is an integer with a padding of 10 bytes and aligned left. specifies the exact length in bytes of the successor data being received
 def getHeaderSize(s):
     # header is 10 bytes
     header = s.recv(HEADER_SIZE).decode("utf-8", "ignore")
     return int(header[:HEADER_SIZE])
 
+
 # receive exact bytes from client in bytes
 def receive(s):
     msg = s.recv(getHeaderSize(s))
     return msg
+
 
 # receive exact bytes from client in UTF-8 format
 def receiveUTF(s):
     msg = s.recv(getHeaderSize(s))
     return msg.decode("utf-8")
 
+
 # ls command function - list all files in current working directory set by cd command
 def ftp_get(command):
-    if(ack[0] == 'D'):  # if file is found on server side
+    if ack[0] == "D":  # if file is found on server side
         fileName = command[4:]
         # explode directory
         # overwrite any files already there
@@ -56,40 +60,51 @@ def ftp_get(command):
         # receive fileSize
         fileSize = int(receiveUTF(sc))
         sizeReceived = 0
-        # print("FILESIZE={fileSize}")
         while sizeReceived != fileSize:
             data = sd.recv(CHUNK_SIZE)
             fullData += data
             sizeReceived += len(data)
-            print("Downloading... {percent}% - {numerator}KB/{denominator}KB".format(percent=int((sizeReceived/fileSize)*100), numerator=sizeReceived/1000, denominator=fileSize/1000, fileSize=fileSize))
-        # print(f"DATA RECV'd = {fullData}")
+            print(
+                "Downloading... {percent}% - {numerator}KB/{denominator}KB".format(
+                    percent=int((sizeReceived / fileSize) * 100),
+                    numerator=sizeReceived / 1000,
+                    denominator=fileSize / 1000,
+                    fileSize=fileSize,
+                )
+            )
         f = open(fileName, "wb")
         f.write(fullData)
         f.close()
-        receiveUTF(sc) # wait for data to be done being sent and
-        print("'{fileName}' successfully downloaded.".format(
-            fileName=fileName))
+        receiveUTF(sc)  # wait for data to be done being sent and
+        print("'{fileName}' successfully downloaded.".format(fileName=fileName))
     else:
         print(ack)
 
+
 # put command function - upload file from client to server with a specified directory
 def ftp_put(command):
-    if(len(command) != 3):
-        fileNameFull = command[4:] # grab full file name with directory
-        index = fileNameFull.rfind("/") # reverse search until first '/' is found to get file name index range
-        fileName = fileNameFull[index+1:] # use reverse search index to assign fileName variable
-        directory = fileNameFull[:index+1] # use reverse search index to assign fileName directory
+    if len(command) != 3:
+        fileNameFull = command[4:]  # grab full file name with directory
+        index = fileNameFull.rfind(
+            "/"
+        )  # reverse search until first '/' is found to get file name index range
+        fileName = fileNameFull[
+            index + 1 :
+        ]  # use reverse search index to assign fileName variable
+        directory = fileNameFull[
+            : index + 1
+        ]  # use reverse search index to assign fileName directory
         found = False
-        try: 
+        try:
             for x in os.listdir(directory):
-                if(x == fileName and not os.path.isdir(x)):
+                if x == fileName and not os.path.isdir(x):
                     found = True
         except FileNotFoundError as e:
             found = False
-        if(found): # if file was found in directory specified
+        if found:  # if file was found in directory specified
             ack = "Downloading {fileName}...".format(fileName=fileName)
             send_data(sc, ack)
-            f = open(fileNameFull, 'rb')
+            f = open(fileNameFull, "rb")
             fileSize = os.path.getsize(fileNameFull)
             sizeSent = 0
             send_data(sc, fileSize)
@@ -98,13 +113,15 @@ def ftp_put(command):
                 sd.send(chunk)
                 sizeSent += len(chunk)
             f.close()
-            receiveUTF(sd) # receive finish message from server
-            print("'{fileName}' was successfully uploaded.".format(
-                fileName=fileName))
+            receiveUTF(sd)  # receive finish message from server
+            print("'{fileName}' was successfully uploaded.".format(fileName=fileName))
         else:
             send_data(sc, "Not found")
-            print("File '{fileName}' was not found in this directory.".format(
-                fileName=fileName))
+            print(
+                "File '{fileName}' was not found in this directory.".format(
+                    fileName=fileName
+                )
+            )
     else:
         print("Invalid syntax. Use: put <filename>")
 
@@ -121,11 +138,11 @@ while True:
     send_data(sc, command)
     # receive acknowledgement from server regarding command
     ack = receiveUTF(sc)
-    if(command[:3] == "get"):  # if command is get
+    if command[:3] == "get":  # if command is get
         ftp_get(command)
-    elif(command[:3] == "put"):  # if command is put
+    elif command[:3] == "put":  # if command is put
         ftp_put(command)
-    elif(command[:4] == "exit"):  # if command is exit
+    elif command[:4] == "exit":  # if command is exit
         ack2 = receiveUTF(sd)
         print(ack)
         print(ack2)
